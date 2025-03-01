dcd09cb2eed2c2945061dbe9fe1d495600bab8ec
Elian Olart <53961725+JernelOlart@users.noreply.github.com>
2025-03-01 13:26:36 -0600

    Add files via upload

---
+++ Note2_YuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note2_YuliApp.swift
+//  Note2_Yuli
+//
+//  Created by Elian Hernández Olarte on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Note2_YuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()  // Asegúrate de que se llama a ContentView aquí
+        }
+    }
+}
